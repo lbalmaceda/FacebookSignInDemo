526c0fa9dceddfc2348a2fc7db3e62b4c547c3bf
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-03-10 18:34:43 -0300

    fetch session token using GraphRequest

--- app/src/main/java/com/auth0/facebooksignin_demo/LoginActivity.kt
+++ app/src/main/java/com/auth0/facebooksignin_demo/LoginActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
+import com.facebook.GraphRequest
 import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -26,8 +26,12 @@ class LoginActivity : AppCompatActivity() {
             setPermissions("email")
             registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult?) {
-                    val fbToken = result?.accessToken?.token
-                    Log.e("Login", "Access Token: $fbToken")
+                    result?.accessToken?.token?.let {
+                        Log.e("Login", "Access Token: $it")
+                        obtainFbSessionToken(it) {
+                            //TODO Use session token
+                        }
+                    }
                 }
 
                 override fun onCancel() {
@@ -43,5 +47,27 @@ class LoginActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun obtainFbSessionToken(fbAccessToken: String, callback: (String?) -> Unit) {
+        val params = Bundle()
+        params.putString("grant_type", "fb_attenuate_token")
+        params.putString("fb_exchange_token", fbAccessToken)
+        params.putString("client_id", getString(R.string.facebook_app_id))
+
+        val request = GraphRequest()
+        request.parameters = params
+        request.graphPath = "oauth/access_token"
+        request.callback = GraphRequest.Callback { response ->
+            if (response.error != null) {
+                // TODO Handle errors
+                callback.invoke(null)
+                return@Callback
+            }
+            val fbSessionToken = response.jsonObject.getString("access_token")
+            Log.e("Login", "Session token: $fbSessionToken")
+            callback.invoke(fbSessionToken)
+        }
+        request.executeAsync()
     }
 }
