8f7a751553e8ec62ad489cbbdc48e5558cd7b2ce
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-03-11 18:54:56 -0300

    add auth0 token exchange request

--- app/src/main/java/com/auth0/facebooksignin_demo/LoginActivity.kt
+++ app/src/main/java/com/auth0/facebooksignin_demo/LoginActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.BaseCallback
+import com.auth0.android.result.Credentials
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
@@ -17,10 +22,17 @@ class LoginActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = LoginActivity::class.simpleName
+        private const val FACEBOOK_SUBJECT_TOKEN_TYPE =
+            "http://auth0.com/oauth/token-type/facebook-info-session-access-token"
         private const val FACEBOOK_PERMISSIONS = "email"
+        private const val AUTH0_SCOPE = "openid email profile offline_access"
     }
 
     private lateinit var fbCallbackManager: CallbackManager
+    private val auth0Client: AuthenticationAPIClient by lazy {
+        val account = Auth0(getString(R.string.a0_app_id), getString(R.string.a0_domain))
+        AuthenticationAPIClient(account)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +49,7 @@ class LoginActivity : AppCompatActivity() {
                         fetchSessionToken(accessToken.token) { sessionToken ->
                             sessionToken?.let {
                                 fetchUserProfile(accessToken.token, accessToken.userId) { profile ->
+                                    exchangeTokens(it, profile)
                                 }
                             }
                         }
@@ -98,6 +111,29 @@ class LoginActivity : AppCompatActivity() {
             callback.invoke(response.rawResponse)
         }
         request.executeAsync()
+    }
+
+    private fun exchangeTokens(sessionToken: String, userProfile: String?) {
+        val params = mapOf("user_profile" to userProfile)
+
+        auth0Client.loginWithNativeSocialToken(sessionToken, FACEBOOK_SUBJECT_TOKEN_TYPE)
+            .setScope(AUTH0_SCOPE)
+            .addAuthenticationParameters(params)
+            .start(object : BaseCallback<Credentials, AuthenticationException> {
+                override fun onSuccess(credentials: Credentials) {
+                    Log.i(TAG, "Logged in")
+                    /*
+                    * Logged in!
+                    *   Use access token to call API
+                    *   or consume ID token locally
+                    */
+                }
+
+                override fun onFailure(error: AuthenticationException) {
+                    Log.e(TAG, "Error ${error.code}: ${error.description}")
+                }
+
+            })
     }
 
 }
